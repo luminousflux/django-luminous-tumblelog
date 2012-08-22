b954f6a29058612ddcb4a8ec07e9542ac81301d4
fin <fin+github@xbhd.org>
2012-08-22 22:40:14 +0200

    better type fields & labels

--- tumblelog/types.py
+++ tumblelog/types.py
@@ -1,12 +1,13 @@
 from django import forms
 
 POST_TYPES = {
-    'photo': {'provider_url': forms.URLField(label='url'),},
+    'photo': {'provider_url': forms.URLField(label='Source URL', required=False),
+              'photo': forms.URLField(label='Photo URL')},
     'video': {'html': forms.CharField(widget=forms.Textarea),},
-    'link': {'provider_url': forms.URLField(label='url'),
+    'link': {'provider_url': forms.URLField(label='Link'),
              'title': forms.CharField(required=False),},
-    'quote': {'provider_url': forms.URLField(label='url'),
-              'title': forms.CharField(required=False),
+    'quote': {'provider_url': forms.URLField(label='Source URL', required=False),
+              'source': forms.CharField(required=False),
               'body': forms.CharField(forms.Textarea),}
 }
 
