04f86d31db670c7edb1ea2cdc1773d36f6616c0b
fin <fin+github@xbhd.org>
2012-09-12 22:49:36 +0200

    fix minor forgotten import

--- tumblelog/models.py
+++ tumblelog/models.py
@@ -10,6 +10,8 @@ from django.template import loader
 
 from jsonfield import JSONField
 
+from datetime import datetime
+
 assert('django_extensions' in settings.INSTALLED_APPS)
 assert('crispy_forms' in settings.INSTALLED_APPS)
 
