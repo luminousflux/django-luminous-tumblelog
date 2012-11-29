2fe1830144239b7b1a23d8760dc09c3c000eed82
Markus Hametner <fin+github@xbhd.org>
2012-11-29 16:15:55 +0100

    better style; add text field for comments

--- tumblelog/__init__.py
+++ tumblelog/__init__.py
@@ -1,1 +1,1 @@
-__version__ = '0.1-fin'
+__version__ = '0.1.1'
--- tumblelog/types.py
+++ tumblelog/types.py
@@ -2,12 +2,16 @@ from django import forms
 
 POST_TYPES = {
     'photo': {'provider_url': forms.URLField(label='Source URL', required=False),
-              'photo': forms.URLField(label='Photo URL')},
-    'video': {'html': forms.CharField(widget=forms.Textarea),},
+              'photo': forms.URLField(label='Photo URL'),
+              'text': forms.CharField(required=False, widget=forms.Textarea),},
+    'video': {'html': forms.CharField(widget=forms.Textarea),
+              'text': forms.CharField(widget=forms.Textarea,required=False),},
     'link': {'provider_url': forms.URLField(label='Link'),
-             'title': forms.CharField(required=False),},
+             'title': forms.CharField(required=False),
+             'text': forms.CharField(widget=forms.Textarea,required=False),},
     'quote': {'provider_url': forms.URLField(label='Source URL', required=False),
               'source': forms.CharField(required=False, label='Source Name'),
-              'body': forms.CharField(widget=forms.Textarea),}
+              'body': forms.CharField(widget=forms.Textarea),
+              'text': forms.CharField(widget=forms.Textarea,required=False),}
 }
 
